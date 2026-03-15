eb6db9cff9b5ca268c35291237eca06b109e7527
Samuel Manda <samuelmanda2002@gmail.com>
2026-03-15 05:06:24 +0200

    fix: vercel proxy, router future flags

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter } from "react-router-dom";
-import Router from "./routes/router";
-import { Header } from "./components/layout/Header";
-import { Footer } from "./components/layout/Footer";
-import { LogPanel } from "./components/ui/LogPanel"; // <-- Import LogPanel
+import { BrowserRouter } from 'react-router-dom';
+import Router from './routes/router';
+import { Header } from './components/layout/Header';
+import { Footer } from './components/layout/Footer';
+import { LogPanel } from './components/ui/LogPanel';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <Header />
       <Router />
       <Footer />
-      <LogPanel /> {/* Add the floating log panel */}
+      <LogPanel />
     </BrowserRouter>
   );
 }
